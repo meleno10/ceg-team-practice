7e7e46fd2946ee1c0f77edeaabf1a31dd5292047
joshuastevenson <stevenson.43@wright.edu>
2016-01-18 14:13:51 -0500

    Created instructions.
    Group members are expected to make a class and make use of it in main

---
+++ readme.md
@@ -0,0 +1,1 @@
+Practice with git. Make a new class with a static method and call it from the main method. Be sure to pull changes and merge before pushing.
--- src/sample/main/Main.java
+++ src/sample/main/Main.java
@@ -1,5 +1,11 @@
 package sample.main;
 
+/**
+ * 
+ * Practice with git. Make a new class with a static method and call it from the
+ * main method. Be sure to pull changes and merge before pushing.  
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
